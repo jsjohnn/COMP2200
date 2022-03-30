c303329af5a8a5739f21d1c5641935ce253d47a8
john kim <jsjohan92@gmail.com>
2022-03-30 18:30:46 +0900

    no message

--- Lab11/data_store.h
+++ Lab11/data_store.h
@@ -2,9 +2,10 @@
 #define DATA_STORE_H
 
 #include <stdbool.h>
+#include "user.h"
 
-user_t* get_user_by_id_or_null(user_t**, size_t);
-user_t* get_user_by_username_or_null(user_t**, const char*);
+user_t* get_user_by_id_or_null(user_t** users_or_null, size_t id);
+user_t* get_user_by_username_or_null(user_t** users_or_null, const char* username);
 bool update_email(user_t** users_or_null, size_t id, const char* email);
 bool update_password(user_t** users_or_null, size_t id, const char* password);
 
